e65c574f45c16f150d5a4ff7bf30791affe7bf63
CaelinusAl <selin.irmak89@gmail.com>
2026-02-07 13:25:58 +0300

    fix: clean main.jsx (remove backend code)

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "@/App";
-import "@/index.css";
-
-from fastapi.middleware.cors import CORSMiddleware
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "https://asksanri-frontend-52xeuimg-caelinus-ai-d01e5346.vercel.app",
-        "https://asksanri.vercel.app",  # varsa custom domain
-        "http://localhost:5173"
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
- 
- 
