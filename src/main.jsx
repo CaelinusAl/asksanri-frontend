0681f53c06b871e24ba3b3174d82efe1519be45e
CaelinusAl <selin.irmak89@gmail.com>
2026-02-09 13:07:50 +0300

    fix: wrap app with context providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+import { LanguageProvider } from "./contexts/LanguageContext";
+import { PremiumProvider } from "./contexts/PremiumContext";
+import { AuthProvider } from "./contexts/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <LanguageProvider>
+          <PremiumProvider>
+            <App />
+          </PremiumProvider>
+        </LanguageProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
