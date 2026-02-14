98e52870091b4231092020a514ad8799a6e2fba2
CaelinusAl <selin.irmak89@gmail.com>
2026-02-15 00:41:51 +0300

    fix: clean admin routes and stabilize app router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-
 // PAGES
 import HomePage from "./pages/HomePage";
 import SanriyaSorPage from "./pages/SanriyaSorPage";
@@ -11,44 +10,38 @@ import FrekansAlaniPage from "./pages/FrekansAlaniPage";
 import RituelAlaniPage from "./pages/RituelAlaniPage";
 import LibraryPage from "./pages/LibraryPage";
 import AwakenedCitiesPage from "./pages/AwakenedCitiesPage";
-import AdminMembersPage from "./pages/AdminMembersPage";
-import AdminPanelPage from "./pages/AdminPanelPage";
-
-
-// YAŞAM KOÇU
 import YasamKocuPage from "./pages/YasamKocuPage";
 
-// (Opsiyonel) Admin
-import AdminLayout from "./components/admin/AdminLayout"; // varsa
-// import AdminDashboard from "./pages/admin/AdminDashboard"; // varsa
+// ADMIN PAGES
+import AdminPanelPage from "./pages/AdminPanelPage";
+import AdminMembersPage from "./pages/AdminMembersPage";
+
+// Eğer gerçekten kullanıyorsan bırak, kullanmıyorsan silebilirsin
+// import AdminLayout from "./components/admin/AdminLayout";
 
 export default function App() {
   return (
     <Routes>
-      {/* HOME / GATES */}
+
+      {/* HOME */}
       <Route path="/" element={<HomePage />} />
 
-      {/* PAGES */}
+      {/* CORE PAGES */}
       <Route path="/sanriya-sor" element={<SanriyaSorPage />} />
       <Route path="/bilinc-alani" element={<BilincAlaniPage />} />
       <Route path="/frekans-alani" element={<FrekansAlaniPage />} />
       <Route path="/rituel-alani" element={<RituelAlaniPage />} />
       <Route path="/library" element={<LibraryPage />} />
       <Route path="/uyanan-sehirler" element={<AwakenedCitiesPage />} />
-      <Route path="/admin-stats" element={<AdminStatsPage />} />
-      <Route path="/admin/members" element={<AdminMembersPage />} />
-      <Route path="/admin/panel" element={<AdminPanelPage />} />
-
-      {/* SANRI YAŞAM KOÇU */}
       <Route path="/yasam-kocu" element={<YasamKocuPage />} />
 
-      {/* ADMIN (varsa) */}
-      <Route path="/admin/*" element={<AdminLayout />}>
-        {/* <Route index element={<AdminDashboard />} /> */}
-      </Route>
+      {/* ADMIN */}
+      <Route path="/admin/panel" element={<AdminPanelPage />} />
+      <Route path="/admin/members" element={<AdminMembersPage />} />
 
       {/* FALLBACK */}
       <Route path="*" element={<Navigate to="/" replace />} />
+
     </Routes>
   );
 }
