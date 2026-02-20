d8a1386316f4d50f1a02ba2fb519358d32ad50be
Selin Irmak <selin.irmak89@gmail.com>
2026-02-20 20:58:00 +0300

    Restore gates routing + keep Sanri page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
 import SanriyaSorPage from "./pages/SanriyaSorPage";
 
+// Eğer bu sayfalar sende varsa aç:
+// import AwakenedCitiesPage from "./pages/AwakenedCitiesPage";
+// import BilincAlaniPage from "./pages/BilincAlaniPage";
+
 export default function App() {
-  return <SanriyaSorPage />;
+  return (
+    <Routes>
+      {/* Kapılar */}
+      <Route path="/" element={<HomePage />} />
+
+      {/* SANRI’ya Sor */}
+      <Route path="/sanri" element={<SanriyaSorPage />} />
+
+      {/* Eski linkler / geri uyumluluk */}
+      <Route path="/sanriya-sor" element={<SanriyaSorPage />} />
+      <Route path="/ask" element={<SanriyaSorPage />} />
+
+      {/* (Opsiyonel) diğer kapılar sende varsa aç */}
+      {/* <Route path="/sehirler" element={<AwakenedCitiesPage />} /> */}
+      {/* <Route path="/bilinc" element={<BilincAlaniPage />} /> */}
+
+      {/* Fallback */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-
 import { LanguageProvider } from "./contexts/LanguageContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import { DoorNavProvider } from "./contexts/DoorNavContext";
