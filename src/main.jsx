729d94526787ade4aa047c8f5fd3dced38d62b42
Selin Irmak <selin.irmak89@gmail.com>
2026-02-20 18:18:12 +0300

    debug: show runtime errors on screen

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,20 @@ import { DoorNavProvider } from "./contexts/DoorNavContext";
 
 import "./index.css";
 
+window.addEventListener("error", (e) => {
+  document.body.innerHTML =
+    "<pre style='white-space:pre-wrap;color:#fff;background:#000;padding:16px;'>" +
+    (e?.error?.stack || e?.message || String(e)) +
+    "</pre>";
+});
+
+window.addEventListener("unhandledrejection", (e) => {
+  document.body.innerHTML =
+    "<pre style='white-space:pre-wrap;color:#fff;background:#000;padding:16px;'>" +
+    (e?.reason?.stack || e?.reason?.message || String(e?.reason || e)) +
+    "</pre>";
+});
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
