db42645dfd5e928df7148dfc7061c956d1428184
Selin Irmak <selin.irmak89@gmail.com>
2026-02-20 19:50:58 +0300

    debug: render App only

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
-
-import { LanguageProvider } from "./contexts/LanguageContext";
-import { AuthProvider } from "./contexts/AuthContext";
-import { DoorNavProvider } from "./contexts/DoorNavContext";
-
 import "./index.css";
 
-window.addEventListener("error", (e) => {
-  document.body.innerHTML =
-    "<pre style='white-space:pre-wrap;color:#fff;background:#000;padding:16px;'>" +
-    (e?.error?.stack || e?.message || String(e)) +
-    "</pre>";
-});
+const rootEl = document.getElementById("root");
+if (!rootEl) throw new Error("root element not found");
 
-window.addEventListener("unhandledrejection", (e) => {
-  document.body.innerHTML =
-    "<pre style='white-space:pre-wrap;color:#fff;background:#000;padding:16px;'>" +
-    (e?.reason?.stack || e?.reason?.message || String(e?.reason || e)) +
-    "</pre>";
-});
-
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
     <BrowserRouter>
-      <LanguageProvider>
-        <AuthProvider>
-          <DoorNavProvider>
-            <App />
-          </DoorNavProvider>
-        </AuthProvider>
-      </LanguageProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
